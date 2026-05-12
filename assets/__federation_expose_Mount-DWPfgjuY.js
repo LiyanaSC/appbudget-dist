5561c67610c3c6a0d80d3122754d0967f357ead5
OPBS Liyana <liyana.perso@gmail.com>
2026-05-12 09:36:31 -0400

    rebuild test nojekyll

---
+++ assets/__federation_expose_Mount-DWPfgjuY.js
@@ -0,0 +1,56 @@
+import { importShared } from './__federation_fn_import-BMMB1g9W.js';
+
+const _export_sfc = (sfc, props) => {
+  const target = sfc.__vccOpts || sfc;
+  for (const [key, val] of props) {
+    target[key] = val;
+  }
+  return target;
+};
+
+const _sfc_main = {  };
+const {createElementVNode:_createElementVNode$1,openBlock:_openBlock$1,createElementBlock:_createElementBlock$1} = await importShared('vue');
+
+
+const _hoisted_1 = { class: "budget-view" };
+
+function _sfc_render(_ctx, _cache) {
+  return (_openBlock$1(), _createElementBlock$1("div", _hoisted_1, _cache[0] || (_cache[0] = [
+    _createElementVNode$1("p", null, "Je suis en cours de développement, je serai bientôt disponible.💜", -1)
+  ])))
+}
+const BudgetView = /*#__PURE__*/_export_sfc(_sfc_main, [['render',_sfc_render],['__scopeId',"data-v-bfd3de19"]]);
+
+const {createElementVNode:_createElementVNode,createVNode:_createVNode,openBlock:_openBlock,createElementBlock:_createElementBlock} = await importShared('vue');
+
+const {createApp} = await importShared('vue');
+
+const {createPinia} = await importShared('pinia');
+function createMyApp(Component, el) {
+  const app = createApp(Component);
+  const pinia = createPinia();
+  app.use(pinia);
+  app.mount(el);
+}
+function mount(el) {
+  if (!el) {
+    console.error("❌ container manquant");
+    return;
+  }
+  el.innerHTML = "";
+  const container = document.createElement("div");
+  container.style.width = "100%";
+  container.style.minHeight = "0";
+  container.style.flex = "1";
+  el.appendChild(container);
+  createMyApp(BudgetView, container);
+}
+window.addEventListener("error", function(event) {
+  console.error("Erreur JavaScript capturée :", event.message);
+  console.log("Fichier :", event.filename);
+  console.log("Ligne :", event.lineno);
+  console.log("Colonne :", event.colno);
+  console.log("Objet erreur complet :", event.error);
+});
+
+export { mount };
--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./mount":()=>{
-      dynamicLoadingCss(["style-sqGwhJzJ.css"], false, './mount');
-      return __federation_import('/appbudget-dist/assets/__federation_expose_Mount-Bl9--Ccr.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-Dpw0EMds.css"], false, './mount');
+      return __federation_import('/appbudget-dist/assets/__federation_expose_Mount-DWPfgjuY.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
