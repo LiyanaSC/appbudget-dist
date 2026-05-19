5d85994c9394c28cca565d0b94405d1ce87a1f73
OPBS Liyana <liyana.perso@gmail.com>
2026-05-19 10:01:55 -0400

    fix: style take off margin top

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./mount":()=>{
-      dynamicLoadingCss(["style-RjBn8D5o.css"], false, './mount');
-      return __federation_import('/appbudget-dist/assets/__federation_expose_Mount-BWlrNOow.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-CzwRTIi5.css"], false, './mount');
+      return __federation_import('/appbudget-dist/assets/__federation_expose_Mount-6nJ62TQL.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
