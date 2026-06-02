5389fd0a03c02f0eac37268dd8d2bf5601ad9d25
OPBS Liyana <liyana.perso@gmail.com>
2026-06-02 15:46:55 -0400

    add new outcomes and upgrade ui/ux

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./mount":()=>{
-      dynamicLoadingCss(["style-tt_wXCWI.css"], false, './mount');
-      return __federation_import('/appbudget-dist/assets/__federation_expose_Mount-DfBCdINu.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-RVi3iBH7.css"], false, './mount');
+      return __federation_import('/appbudget-dist/assets/__federation_expose_Mount-CxkJAyd9.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
