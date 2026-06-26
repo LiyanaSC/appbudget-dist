23430425946b7d5533ddda698d3cf3212f17bc87
OPBS Liyana <liyana.perso@gmail.com>
2026-06-26 12:13:09 -0400

    Make sidebar and icons theme

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./mount":()=>{
-      dynamicLoadingCss(["style-BuC1mfVj.css"], false, './mount');
-      return __federation_import('/appbudget-dist/assets/__federation_expose_Mount-ot55Lj6z.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-DSN2Fxc6.css"], false, './mount');
+      return __federation_import('/appbudget-dist/assets/__federation_expose_Mount-od3A1LRV.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
